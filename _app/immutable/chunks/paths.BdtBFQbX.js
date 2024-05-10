503dac2f05dfd8660912a84c6f67aebf3ae3f29d
Anofio <kira0void@proton.me>
2024-05-10 21:40:44 +0300

    Fix paths

---
+++ _app/immutable/chunks/paths.BdtBFQbX.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1jpv45x)==null?void 0:s.base)??"/home";var e;const t=((e=globalThis.__sveltekit_1jpv45x)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.f83-Te0F.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.3YhhUj36.js";export{t as start};
